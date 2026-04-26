0e0c78f11626bba1bcc62cbc35487044dad115aa
X-MEN-Cyclops-2026 <ahnaaf29@outlook.com>
2026-04-26 17:19:38 -0600

    Today,, I have finished the puzzle gaming assignment by removing the gradient effect and change the color of the overlay color to red since its my favorite color. But I didnt need the gradient since its already looking simple but also complex

--- Puzzle Game Assignment/sketch.js
+++ Puzzle Game Assignment/sketch.js
@@ -21,43 +21,25 @@ function setup() {
 }
 
 function draw() { // this functions renders the whole grid, and draws the background,
-  background(255); // also adds th     e abilities and special effects to the grid
+  background(255); // also adds the abilities and special effects to the grid
   renderGrid();
-  blendMode(ADD);
-  canvasEffects();
-  blendMode(BLEND)
   overlay();
   textSize(30);
   fill(0, 255, 255);
   if (winCondition()) {
     textSize(150);
     text("YOU WON", width / 4, height / 3);
-  }
-  //text(getCurrentX() + ", " + getCurrentY(), mouseX, mouseY);
+  } //text(getCurrentX() + ", " + getCurrentY(), mouseX, mouseY);
 }
 
 
 
 //Challenges Features 
-function canvasEffects() { // this is a function that provides a special effect
-  noStroke();  // on the looks of the entire puzzle
-  for (let y = 0; y < height; y++) {
-    let mapping = map(y, 0, height, 0, 1);
-    let c = lerpColor(
-      color(20, 0, 40),   // dark purple
-      color(255, 0, 120), // neon pink
-      mapping
-    );
-    stroke(c);
-    line(0, y, width, y);
-  }
-}
-
-function overlay() { // This function allows to see which parts of
-  let x = getCurrentX(); // the puzzle are going to be flipped when its is clicked
+function overlay() { // This function allows to see which parts of the puzzle are going to be flipped when its is clicked
+  let x = getCurrentX(); 
   let y = getCurrentY();
 
-  fill(20, 150, 255, 120);
+  fill(250, 50, 250, 120);
 
   // Always draw center
   rect(x * tileSize, y * tileSize, tileSize);
@@ -83,7 +65,7 @@ function overlay() { // This function allows to see which parts of
 
 
 //Basic Features 
-function flip(x,y){
+function flip(x,y){ //this function allows the tiles/squares flip their colors which clicked
   if(grid[y][x] === 0) grid[y][x] = 255;
   else grid[y][x] = 0;
 }
@@ -98,7 +80,6 @@ function mousePressed(){
     // ALWAYS:
     flip(x, y);
 
-    // IF THEY EXIST:
     // flip the cardinal (NSEW) neighbours
     if(x-1 >= 0) flip(x-1, y);
     if(y-1 >= 0) flip(x, y-1);
@@ -106,9 +87,7 @@ function mousePressed(){
   
 }
 
-function renderGrid(){
-  // intepret the data stored in 2D array (grid) and
-  // draw a matrix of squares to reflect it
+function renderGrid(){ // this explains the data stored in 2D array (grid) and draw a matrix of squares to reflect it
   for(let y = 0; y < rows; y++){ //y:0 1 2 3 4
     for(let x = 0; x < cols; x++){ //x: 0 1 2 3 4 5
       let fillColor = grid[y][x];
@@ -118,19 +97,17 @@ function renderGrid(){
   }
 }
 
-function getCurrentX(){
-  //determine the current col position of mouse
+function getCurrentX(){ //this determines the current column position of the user's mouse
   let constrainedX = constrain(mouseX, 0, width-1);
   return floor(constrainedX / tileSize);
 }
 
-function getCurrentY(){
-  //determine the current row position of mouse
+function getCurrentY(){ //This determines the current row position of the user's mouse
   let constrainedY = constrain(mouseY, 0, height-1);
   return floor(constrainedY / tileSize);
 }
 
-function randomizeGrid() {  // this functions allow to randomize the grid squares 
+function randomizeGrid() {  // this functions allows to randomize the grid squares 
   for (let y = 0; y < rows; y++) {
     for (let x = 0; x < cols; x++) {
       grid[y][x] = random([0, 255]); // this helps to pick black or white randomly
@@ -142,17 +119,18 @@ function randomizeGrid() {  // this functions allow to randomize the grid square
 }
 
 function winCondition() {
-  // this functions alllow the user to know if they have "won" by solving
-  // the puzzle. And it checks all the tiles starting at the top and the corners of all the grid.
+  // this functions allow the user to know if they have "won" by solving the puzzle. which then checks all the tiles/squares 
+  // starting at the top and the corners of all the grid.
   let wholeGrid = grid[0][0];
   for (let y = 0; y < rows; y++) { // y: 0 1 2 3 4
     for (let x = 0; x < cols; x++) { // x: 0 1 2 3 4 5
       if (grid[y][x] !== wholeGrid) {
-        win = false; // changes to when not all the tiles are black or white
-        return win; // this return allows us to continue making the changes
-      } // necessary when the puzzle is still not solved
+        win = false; // this changes to when not all the tiles/squares are black or white after the click
+        return win; // this return, allows us to continue making the changes necessary and when the puzzle is still not solved.
+      } 
+
       else {
-        win = true; // This changes to when all the squares/tiles are black or white
+        win = true; // This changes to when every squares/tiles are black or white
       }
     }
   }
